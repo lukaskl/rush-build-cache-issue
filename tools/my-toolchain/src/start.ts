04dedd16943e2850db33758d6110fbdafda1ff57
Lukas <lukas@klusis.com>
2022-06-22 13:05:04 +0300

    Add build:istanbul command

--- tools/my-toolchain/src/start.ts
+++ tools/my-toolchain/src/start.ts
@@ -1,10 +1,16 @@
 #!/usr/bin/env node
 
-import * as colors from 'colors';
-import * as child_process from 'child_process';
+import * as colors from 'colors'
+import * as child_process from 'child_process'
 
-console.log('Invoking my-toolchain...');
+const isIstanbulBuild = process.argv.some((a) => a.includes('--istanbul'))
 
-child_process.execSync('tsc', { stdio: 'inherit' });
+console.log('Invoking my-toolchain...')
 
-console.log(colors.green('Success!'));
+// This is a dummy implementation
+// We don't use istanbul or anything, just outputting to a different directory
+// This whole repo is just for demo purposes and to report an issue.
+const command = isIstanbulBuild ? 'tsc --outDir lib-istanbul' : 'tsc'
+child_process.execSync(command, { stdio: 'inherit' })
+
+console.log(colors.green('Success!'))
